ddec6ae1639e58ceac8ef2911ea4f332ba9622d2
Nicola Iantomasi <info@yimp.it>
2025-09-25 16:15:14 +0200

    Add files via upload

--- Workshop 18 settembre soluzione.sql
+++ Workshop 18 settembre soluzione.sql
@@ -19,11 +19,15 @@ Se proviamo nuovamente la INSERT. Otterremo un errore di chiave primaria duplica
 Scrivere la query per individuare le coppie OrderId e ProductID duplicate.
 Per risolvere il problema, a parità di OrderId e ProductID, inserire soltanto la riga con RowID maggiore.
 Suggerimento: aiutiamoci con la Window Function RANK(). Inseriamo prima la RANK nella SELECT di una CTE (attenzione a cosa mettere nella OVER). Poi eseguiamo il filtro sulla CTE dove la WindowFunction vale 1.
+
 Step 5 
-Creare una procedura con gestione degli errori e delle transazioni che cancelli le righe esistenti nella CleanedOrders e poi esegua la INSERT dei dati.
+Creare una procedura con gestione degli errori e delle transazioni 
+che cancelli le righe esistenti nella CleanedOrders 
+e poi esegua la INSERT dei dati.
 
 Step 6
-Modificare lo step 5 in modo che le righe cancellate vengano scritte nella tabella CleanedOrdersLog.
+Modificare lo step 5 in modo che le righe 
+cancellate vengano scritte nella tabella CleanedOrdersLog.
 
 Step 7
 Creare una nuova procedura che, partendo da Orders, inserisca le nuove righe nella CleanedOrders2, aggiorni quelle precedenti impostando FlagAttivo a 0 e la DataFineValidità pari alla nuova DataInizioValidità
@@ -41,7 +45,16 @@ Step 11
 Creare una nuova procedura che, partendo dalla tabella Orders2, in base alla chiave OrderId e ProductId, inserisca sia le nuove righe e sia quelle con delle modifiche rispetto alla CleanedOrders2. Inoltre occorre aggiornare le versioni precedenti modificate con FlagAttivo a 0 e la DataFineValidità pari alla nuova DataInizioValidità
 */
 
-/*Step 1*/
+/*ATTENZIONE ALLE DIFFERENZE TRA DELETE E TRUNCATE!*/
+
+/*ATTENZIONE AL LIVELLO DI ISOLAMENTO READ COMMITTED SNAPSHOT.
+IMPATTA SULLA DECISIONE DI SALVARE I DATI IN UNA TABELLA TEMPORANEA
+PRIMA DELL'APERTURA DELLA TRANSAZIONE*/
+
+/*ATTENZIONE A USARE LE TRANSAZIONI SOLO QUANDO RICHIESTO DAL
+PROCESSO (SOPRATTUTTO SE DEVO CANCELLARE E INSERIRE TANTI RECORD)*/
+
+/*Step 1,2,3,4*/
 DECLARE @data_inizio_validita DATETIME = GETDATE();
 
 WITH CTE AS (
@@ -92,3 +105,182 @@ SELECT RowID,OrderID,OrderDate,ShipDate,ShipMode
     ,Quantity,Discount,Profit,DataInizioValidità
     ,DataFineValidità,FlagAttivo
 FROM Cte
+WHERE RN = 1;
+
+go
+/*Step 5*/
+CREATE OR ALTER PROCEDURE projectwork.LoadCleanedOrders
+AS
+SET XACT_ABORT ON
+SET NOCOUNT ON
+BEGIN
+    BEGIN TRY
+    DECLARE @data_inizio_validita DATETIME = GETDATE();
+
+    WITH CTE AS (
+        SELECT 
+            O.RowID,
+            O.OrderID,
+            CONVERT(DATE,O.OrderDate,103) AS OrderDate,
+            CONVERT(DATE,O.ShipDate,103) AS ShipDate,   
+            O.ShipMode,
+            O.CustomerID,
+            O.CustomerName,
+            O.Segment,
+            O.Country,
+            O.City,
+            O.State,
+            O.PostalCode,
+            O.Region,
+            O.ProductID,
+            O.Catery,
+            O.SubCatery,
+            O.ProductName,
+            CONVERT(DECIMAL(18,4),REPLACE(O.Sales,',','.')) AS Sales,
+            O.Quantity, 
+            CONVERT(DECIMAL(18,4),REPLACE(O.Discount,',','.')) AS Discount,
+            CONVERT(DECIMAL(18,4),REPLACE(O.Profit,',','.')) AS Profit,
+            @data_inizio_validita AS DataInizioValidità,
+            NULL AS DataFineValidità,
+            1 AS FlagAttivo,
+            ROW_NUMBER() OVER (PARTITION BY O.OrderId, O.ProductId
+                               ORDER BY O.RowId DESC) AS RN
+        FROM 
+        projectwork.Orders AS O
+        LEFT JOIN projectwork.Returns AS R
+	        ON O.OrderId = R.OrderID
+        WHERE R.OrderID IS NULL
+        )
+    SELECT RowID,OrderID,OrderDate,ShipDate,ShipMode
+        ,CustomerID,CustomerName,Segment,Country
+        ,City,State,PostalCode,Region,ProductID
+        ,Catery,SubCatery,ProductName,Sales
+        ,Quantity,Discount,Profit,DataInizioValidità
+        ,DataFineValidità,FlagAttivo
+    INTO #TAB
+    FROM Cte
+    WHERE RN = 1;
+
+    BEGIN TRAN
+
+        TRUNCATE TABLE CorsoSQL.projectwork.CleanedOrders;
+
+        INSERT INTO CorsoSQL.projectwork.CleanedOrders
+                   (RowID,OrderID,OrderDate,ShipDate,ShipMode
+                   ,CustomerID,CustomerName,Segment,Country
+                   ,City,State,PostalCode,Region,ProductID
+                   ,Catery,SubCatery,ProductName,Sales
+                   ,Quantity,Discount,Profit,DataInizioValidità
+                   ,DataFineValidità,FlagAttivo)
+        SELECT *
+        FROM   #TAB;
+    COMMIT
+    END TRY
+    BEGIN CATCH
+        IF @@TRANCOUNT > 0
+        BEGIN
+            ROLLBACK;
+        END;
+
+        THROW;
+    END CATCH
+END
+
+go
+/*Step 6 */
+CREATE OR ALTER PROCEDURE projectwork.LoadCleanedOrders
+AS
+SET XACT_ABORT ON
+SET NOCOUNT ON
+BEGIN
+    BEGIN TRY
+
+    DECLARE @data_inizio_validita DATETIME = GETDATE();
+
+    WITH CTE AS (
+        SELECT 
+            O.RowID,
+            O.OrderID,
+            CONVERT(DATE,O.OrderDate,103) AS OrderDate,
+            CONVERT(DATE,O.ShipDate,103) AS ShipDate,   
+            O.ShipMode,
+            O.CustomerID,
+            O.CustomerName,
+            O.Segment,
+            O.Country,
+            O.City,
+            O.State,
+            O.PostalCode,
+            O.Region,
+            O.ProductID,
+            O.Catery,
+            O.SubCatery,
+            O.ProductName,
+            CONVERT(DECIMAL(18,4),REPLACE(O.Sales,',','.')) AS Sales,
+            O.Quantity, 
+            CONVERT(DECIMAL(18,4),REPLACE(O.Discount,',','.')) AS Discount,
+            CONVERT(DECIMAL(18,4),REPLACE(O.Profit,',','.')) AS Profit,
+            @data_inizio_validita AS DataInizioValidità,
+            NULL AS DataFineValidità,
+            1 AS FlagAttivo,
+            ROW_NUMBER() OVER (PARTITION BY O.OrderId, O.ProductId
+                               ORDER BY O.RowId DESC) AS RN
+        FROM 
+        projectwork.Orders AS O
+        LEFT JOIN projectwork.Returns AS R
+	        ON O.OrderId = R.OrderID
+        WHERE R.OrderID IS NULL
+        )
+    SELECT RowID,OrderID,OrderDate,ShipDate,ShipMode
+        ,CustomerID,CustomerName,Segment,Country
+        ,City,State,PostalCode,Region,ProductID
+        ,Catery,SubCatery,ProductName,Sales
+        ,Quantity,Discount,Profit,DataInizioValidità
+        ,DataFineValidità,FlagAttivo
+    INTO #TAB
+    FROM Cte
+    WHERE RN = 1;
+
+    BEGIN TRAN
+
+        DELETE 
+        FROM   CorsoSQL.projectwork.CleanedOrders
+        OUTPUT deleted.RowID,deleted.OrderID,deleted.OrderDate,
+                     deleted.ShipDate,deleted.ShipMode
+                   ,deleted.CustomerID,deleted.CustomerName,deleted.Segment,deleted.Country
+                   ,deleted.City,deleted.State,deleted.PostalCode,deleted.Region,deleted.ProductID
+                   ,deleted.Catery,deleted.SubCatery,deleted.ProductName,deleted.Sales
+                   ,deleted.Quantity,deleted.Discount,deleted.Profit,deleted.DataInizioValidità
+                   ,@data_inizio_validita as DataFineValidità
+        INTO   CorsoSQL.projectwork.CleanedOrdersLog;
+
+
+        INSERT INTO CorsoSQL.projectwork.CleanedOrders
+                   (RowID,OrderID,OrderDate,ShipDate,ShipMode
+                   ,CustomerID,CustomerName,Segment,Country
+                   ,City,State,PostalCode,Region,ProductID
+                   ,Catery,SubCatery,ProductName,Sales
+                   ,Quantity,Discount,Profit,DataInizioValidità
+                   ,DataFineValidità,FlagAttivo)
+        SELECT *
+        FROM   #TAB;
+
+    COMMIT
+    END TRY
+    BEGIN CATCH
+        IF @@TRANCOUNT > 0
+        BEGIN
+            ROLLBACK;
+        END;
+
+        THROW;
+    END CATCH
+
+END
+
+/*Test
+select * from projectwork.CleanedOrders 
+select * from projectwork.CleanedOrderslog where rowid = 5069
+order by DataInizioValidità
+exec projectwork.LoadCleanedOrders
+*/
