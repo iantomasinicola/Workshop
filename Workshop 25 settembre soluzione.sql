44cad959c65328a51bd136762d4dda7d32cdfd4c
Nicola Iantomasi <info@yimp.it>
2025-10-02 15:37:30 +0200

    Update Workshop 25 settembre soluzione.sql

--- Workshop 25 settembre soluzione.sql
+++ Workshop 25 settembre soluzione.sql
@@ -49,14 +49,39 @@ FROM
 ORDER BY COUNT(*) desc
 
 
---SOLUZIONE1
+/*************************************
+Lavoriamo con tutte le righe di test
+*************************************/
+
+--Strategia 1: vista parametrica + cross apply	
+CREATE FUNCTION dbo.PredizioneIris1 (@rownumber INT)
+RETURNS TABLE 
+RETURN
+SELECT TOP 1  
+	z.predizione
+FROM 
+(
+SELECT TOP 5 a.class as predizione,
+	 CONVERT(DECIMAL(8,4),power(a.[sepal_length]-B.sepal_length,2) + 
+	      POWER(a.sepal_width-B.sepal_width,2)+
+		  POWER(a.[petal_length]-B.[petal_length],2) + 
+		  POWER(a.[petal_width]-B.[petal_width],2)) as punteggio
+FROM [dbo].[IrisTraining]  as A
+CROSS JOIN (select * from  [dbo].[IrisTest] where rownumber = @rownumber) as B
+ORDER BY punteggio ASC
+
+  ) as z
+  GROUP BY z.predizione
+ORDER BY COUNT(*) desc
+
+	
 SELECT T.CLASS AS RealClass,
 	p.predizione
 FROM  (SELECT T1.Rownumber, T1.CLASS 
 		FROM  [dbo].[IrisTest]  AS T1) AS T
 CROSS APPLY  dbo.PredizioneIris1(T.Rownumber) AS P;
 
---SOLUZIONE2
+--Strategia 2: window function lavorando tutti i dati insieme
 with 
   cte as (
 	SELECT 
@@ -91,4 +116,49 @@ FROM   #PRIMI5_con_predizione3)
 SELECT  *
 FROM   CTE
 WHERE  RN = 1
+
+--Strategia 3: cursore
+declare @row_number int; 
+declare @sepal_length decimal(18,4); 
+declare @sepal_width decimal(18,4); 
+declare @petal_length decimal(18,4); 
+declare @petal_width decimal(18,4);
+declare @class varchar(50);
+
+DECLARE cursore CURSOR
+	FOR select rownumber, sepal_length, sepal_width,
+			[petal_length],[petal_width],class
+		from  [dbo].[IrisTest];
+
+open cursore;
+
+fetch next from cursore into @row_number, @sepal_length,@sepal_width,@petal_length,@petal_width, @class
+
+while (@@FETCH_STATUS = 0)
+begin
+	insert into #output
+	SELECT TOP 1  
+		z.predizione, @class as classe_reale
+	FROM 
+	(
+	SELECT TOP 5 a.class as predizione,
+		 CONVERT(DECIMAL(8,4),power(a.[sepal_length]-@sepal_length,2) + 
+			  POWER(a.sepal_width-@sepal_width,2)+
+			  POWER(a.[petal_length]-@petal_length,2) + 
+			  POWER(a.[petal_width]-@petal_width,2)) as punteggio
+	FROM [dbo].[IrisTraining]  as A
+	ORDER BY punteggio ASC
+
+	  ) as z
+	  GROUP BY z.predizione
+	ORDER BY COUNT(*) desc;
+
+	fetch next from cursore into @row_number, @sepal_length,@sepal_width,@petal_length,@petal_width, @class
+
+end
+
+close cursore;
+deallocate cursore;
+
+
 
