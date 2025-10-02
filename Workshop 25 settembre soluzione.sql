aedf10cfd551ec177b4a25db89df85ae41fcbf64
Nicola Iantomasi <info@yimp.it>
2025-10-02 15:04:25 +0200

    Update Workshop 25 settembre soluzione.sql

--- Workshop 25 settembre soluzione.sql
+++ Workshop 25 settembre soluzione.sql
@@ -47,3 +47,48 @@ FROM
   ) as z
   GROUP BY z.predizione
 ORDER BY COUNT(*) desc
+
+
+--SOLUZIONE1
+SELECT T.CLASS AS RealClass,
+	p.predizione
+FROM  (SELECT T1.Rownumber, T1.CLASS 
+		FROM  [dbo].[IrisTest]  AS T1) AS T
+CROSS APPLY  dbo.PredizioneIris1(T.Rownumber) AS P;
+
+--SOLUZIONE2
+with 
+  cte as (
+	SELECT 
+		b.rownumber as riga_di_test,
+		a.class as possibile_predizione,
+		 CONVERT(DECIMAL(8,4),power(a.[sepal_length]-B.sepal_length,2) + 
+			  POWER(a.sepal_width-B.sepal_width,2)+
+			  POWER(a.[petal_length]-B.[petal_length],2) + 
+			  POWER(a.[petal_width]-B.[petal_width],2)) as punteggio
+	FROM [dbo].[IrisTraining]  as A
+	CROSS JOIN  [dbo].[IrisTest]  as B),
+  cte2 as (
+	SELECT 
+		*,
+		ROW_NUMBER() OVER(PARTITION BY RIGA_DI_TEST
+						  ORDER BY punteggio asc) as rn
+	FROM cte)
+SELECT RIGA_DI_TEST,
+	POSSIBILE_PREDIZIONE,
+	COUNT(*) AS FrequenzaPredizione
+INTO   #PRIMI5_con_predizione3
+FROM   CTE2
+WHERE  RN <= 5
+GROUP BY RIGA_DI_TEST,
+	POSSIBILE_PREDIZIONE;
+
+WITH CTE AS (
+SELECT *,
+	ROW_NUMBER() OVER(PARTITION BY RIGA_DI_TEST
+					  ORDER BY FrequenzaPredizione DESC) AS RN
+FROM   #PRIMI5_con_predizione3)
+SELECT  *
+FROM   CTE
+WHERE  RN = 1
+
