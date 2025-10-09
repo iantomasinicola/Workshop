157576a80bcf7c9631ac06f733f75a63341e686d
Nicola Iantomasi <info@yimp.it>
2025-10-09 14:48:07 +0200

    Update Workshop 02 ottobre soluzione.sql

--- ottobre/Workshop 02 ottobre soluzione.sql
+++ ottobre/Workshop 02 ottobre soluzione.sql
@@ -6,6 +6,8 @@ CREATE TABLE dbo.Utenti(
 	DataRegistrazione DATETIME NOT NULL,
 	PRIMARY KEY (IdUtente));
 
+
+
 /*Creazione tabella da entità Post*/
 CREATE TABLE dbo.Post(
 	IdPost INT NOT NULL,
@@ -49,4 +51,58 @@ CREATE TABLE dbo.Follow(
 		references  dbo.Utenti(IDUTENTE),
 	foreign key (id_utente_following) 
 		references  dbo.Utenti(IDUTENTE),
-	CHECK (id_utente_follower != id_utente_following) )
+	CHECK (id_utente_follower != id_utente_following)
+	);
+
+
+
+CREATE TABLE dbo.Commenti(
+	IdCommento INT NOT NULL,
+	contenuto NVARCHAR(MAX) NOT NULL,
+	data_commento datetime not null,
+	IDUTENTE int not null,
+	IdPost int not null,
+	IdCommentoRisposta int null,
+	primary key(IdCommento),
+	foreign key(IDUTENTE) 
+		references  dbo.Utenti(IDUTENTE),
+	foreign key(IdPost) 
+		references  dbo.Post(IdPost),
+	foreign key(IdCommentoRisposta) 
+		references  dbo.Commenti(IdCommento));
+
+--esempi con colonne bit. - è convertito in 0 mentre 2 è convertito in 1
+CREATE TABLE #T(COL1 BIT)
+
+INSERT INTO #T(COL1)
+SELECT '-'
+
+INSERT INTO #T(COL1)
+SELECT '2'
+
+--esempi con colonne varchar e nvarchar.
+--caratteri particolari vanno inseriti in colonne nvarchar, 
+--specificando N prima dell'apertura degli apici
+CREATE TABLE  #test (Col1 varchar(50), col2 nvarchar(50))
+
+INSERT INTO #test (col1, col2)
+SELECT 'こんにちは', N'こんにちは'
+
+SELECT *
+FROM #test;
+
+--Senza N il dato non è inserito correttamente
+INSERT INTO #test (col1, col2)
+SELECT 'こんにちは', 'こんにちは'
+
+--la N serve anche in fase di filtro con la colonna NVARCHAR
+SELECT COUNT( *)
+FROM #TEST
+WHERE col2 = N'こんにちは';
+
+
+--la N usata per fare un filtro con una colonna VARCHAR impedisce di usare 
+--un indice efficientemente
+SELECT COUNT( *)
+FROM #TEST
+WHERE col2 = 'こんにちは';
